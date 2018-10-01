7ca9feb07a63b86928bc4549e6ae21be9f5f392f
Austin Wright <aaa@bzfx.net>
2018-10-01 14:44:30 -0700

    Support multiple alternatives for First

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ module.exports.First = require('./lib/http.js').First;
 module.exports.Processor = require('./lib/http.js').Processor;
 
 module.exports.TemplateRouter = require('uri-template-router');
-module.exports.markdown = require( "markdown" ).markdown;
 
 module.exports.RouteNotFound = require('./lib/RouteNotFound.js');
 module.exports.RouteStaticFile = require('./lib/RouteStaticFile.js').RouteStaticFile;
