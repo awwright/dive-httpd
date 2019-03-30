533f4f27c39fadc917d72093fc46959e0d5f16c6
Austin Wright <aaa@bzfx.net>
2019-03-29 21:55:50 -0700

    Add tests for First#prepare, First#watch

--- test/First.test.js
+++ test/First.test.js
@@ -7,14 +7,54 @@ describe('First', function(){
 		var route;
 		beforeEach(function(){
 			route = lib.First([
-				new lib.RouteStaticFile(__dirname+'/RouteStaticFile-data', "{/path*}.html", 'text/html'),
-				new lib.RouteStaticFile(__dirname+'/RouteStaticFile-data', "{/path*}.xhtml", 'application/xhtml+xml'),
-				new lib.RouteStaticFile(__dirname+'/RouteStaticFile-data', "{/path*}.txt", 'text/plain'),
+				new lib.RouteStaticFile({
+					uriTemplate: 'http://example.com{/path*}.html',
+					contentType: 'text/html',
+					fileroot: __dirname+'/RouteStaticFile-data',
+					pathTemplate: "{/path*}.html",
+				}),
+				new lib.RouteStaticFile({
+					uriTemplate: 'http://example.com{/path*}.xhtml',
+					contentType: 'application/xhtml+xml',
+					fileroot: __dirname+'/RouteStaticFile-data',
+					pathTemplate: "{/path*}.xhtml",
+				}),
+				new lib.RouteStaticFile({
+					uriTemplate: 'http://example.com{/path*}.txt',
+					contentType: 'text/plain',
+					fileroot: __dirname+'/RouteStaticFile-data',
+					pathTemplate: "{/path*}.txt",
+				}),
 			]);
 		});
-		it('First#name');
-		it('First#prepare');
-		it('First#watch');
+		it('First#name', function(){
+			assert.strictEqual(route.name.substring(0,5), 'First');
+		});
+		it('First#prepare (200, data-table.html)', function(){
+			return route.prepare('http://example.com/data-table.html').then(function(res){
+				assert(res instanceof lib.Resource);
+			});
+		});
+		it('First#prepare (200, document.xhtml)', function(){
+			return route.prepare('http://example.com/document.xhtml').then(function(res){
+				assert(res instanceof lib.Resource);
+			});
+		});
+		it('First#prepare (404)', function(){
+			return route.prepare('http://example.com/data-table.txt').then(function(res){
+				assert(!res);
+			});
+		});
+		it('First#watch', function(done){
+			var count = 0;
+			route.watch(function(data, filepath){
+				count++;
+				if(data.path && data.path.length===2 && data.path[0]==='directory' && data.path[1]==='data-table'){
+					return void done();
+				}
+				// if(count>=2) assert.fail();
+			});
+		});
 		it('First#listing', function(){
 			return route.listing().then(function(listing){
 				// console.log(listing);
