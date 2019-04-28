e7976bfe56186b89efc8bcb08872e6aa8b290fdd
Austin Wright <aaa@bzfx.net>
2019-04-27 21:04:50 -0700

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"env": {
+		"node": true,
+		"es6": true,
+	},
+	"extends": "eslint:recommended",
+	"parserOptions": {
+		"ecmaVersion": 6,
+	},
+	"rules": {
+		"indent": [ "error", "tab", { SwitchCase: 1 } ],
+		"no-unused-vars": [ "warn" ],
+		"no-unreachable": [ "error" ],
+		"linebreak-style": [  "error", "unix" ],
+		//"semi": [ "error", "always" ],
+		"comma-dangle": [ "error", "always-multiline" ],
+		"no-console": [ "error" ],
+	}
+};
