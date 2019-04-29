e5a35c43f1b97c52540ace4dcbddb63eeaac1245
Austin Wright <aaa@bzfx.net>
2019-04-29 01:13:16 -0700

    Add test stubs for Gateway

---
+++ test/Gateway.test.js
@@ -0,0 +1,22 @@
+
+var assert = require('assert');
+
+var testMessage = require('../../dive-httpd/test/util.js').testMessage;
+var lib = require('../../dive-httpd/index.js');
+
+describe('Gateway', function(){
+	describe('interface', function(){
+		var route;
+		beforeEach(function(){
+			route = lib.Gateway();
+		});
+		it('Gateway#label', function(){
+			assert.strictEqual(route.label, 'Gateway');
+		});
+		it('Gateway#prepare');
+		it('Gateway#error');
+		it('Gateway#watch');
+		it('Gateway#listing');
+		it('Gateway#store');
+	});
+});
