012076a1359a9cbbce9f41b6e6ea8e59cfe35a7a
Austin Wright <aaa@bzfx.net>
2019-04-30 16:58:02 -0700

    Rewrite Gateway
    And get rid of Route#request, there's no way for it to play nice with Route#prepare

--- test/Gateway.test.js
+++ test/Gateway.test.js
@@ -1,4 +1,5 @@
 
+var http = require('http');
 var assert = require('assert');
 
 var testMessage = require('../../dive-httpd/test/util.js').testMessage;
@@ -8,18 +9,57 @@ describe('Gateway', function(){
 	describe('interface', function(){
 		var route;
 		beforeEach(function(){
-			route = lib.Gateway();
+			route = lib.Gateway({
+				uriTemplate: 'http://localhost/{path}'
+			});
 		});
 		it('Gateway#label', function(){
 			assert.strictEqual(route.label, 'Gateway');
 		});
-		it('Gateway#prepare');
+		it('Gateway#prepare', function(){
+			return route.prepare('http://localhost/foo').then(function(resource){
+				assert(resource instanceof lib.Resource);
+			});
+		});
 		it('Gateway#error');
 		it('Gateway#watch');
 		it('Gateway#listing');
 		it('Gateway#store');
 		it('Gateway#listDependents', function(){
 			assert(route.listDependents().length);
+		});
+	});
+	describe('app', function(){
+		var app, originServer;
+		before(function(){
+			originServer = http.createServer(function(req, res){
+				res.setHeader('Content-Type', 'text/plain');
+				res.end(req.url);
+			}).listen(0);
+			var originAddress = originServer.address();
+
+			app = new lib.HTTPServer;
+			app.addRoute(lib.Gateway({
+				uriTemplate: 'http://example.com/{+foo}',
+				remoteHost: originAddress.address,
+				remotePort: originAddress.port,
+			}));
+			app.onError = function handleError(req, err){
+				throw err;
+			}
+		});
+		after(function(){
+			// FIXME use a stream or something
+			originServer.close();
+		});
+		it('Request', function(){
+			return testMessage(app, [
+				'GET http://example.com/test-path HTTP/1.1',
+				'Host: example.com',
+			]).then(function(res){
+				assert(res.toString().match(/^HTTP\/1.1 200 /));
+				assert(res.toString().match(/test-path/));
+			});
 		});
 	});
 });
