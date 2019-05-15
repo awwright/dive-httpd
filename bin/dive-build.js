4f72dd592db27da2f99a882343b63fa1fa25610d
Austin Wright <aaa@bzfx.net>
2019-05-14 19:42:18 -0700

    Improve dive-build reporting

--- bin/dive-build.js
+++ bin/dive-build.js
@@ -3,28 +3,44 @@
 const fs = require('fs');
 const path = require('path');
 const mkdir = require('fs').promises.mkdir;
+const dotenv = require('dotenv');
 
 const args = require('commander');
 const { PassThrough } = require('stream');
 
 args.usage('[-c app.conf] [options] [targets...]', 'Read <app.conf> and build targets');
-args.option('-c <app.conf>', 'Use task directives from <app.conf>');
+// args.option('-c <app.conf>', 'Use task directives from <app.conf>');
 args.option('-f <app.js>', 'Import application file from <app.js>');
+args.option('--env <file.env>', 'Import environment variables from <file.env> instead of .env');
 args.option('--all', 'Build all resources as targets');
 args.option('--base <uri>', 'Map files output underneath <uri> into the target dir');
-args.option('--clean', 'Delete the targets');
 args.option('--target <dir>', 'Write files to <dir> instead of CWD');
-args.option('-j<n>, --jobs <n>', 'Allow <n> jobs at once');
+// args.option('--watch', 'Hold process open and rebuild changed resources');
+// args.option('--clean', 'Delete the targets');
+// args.option('-j<n>, --jobs <n>', 'Allow <n> jobs at once');
+// args.option('--delete', 'Delete all other resources under the base');
 args.option('--n, --pretend, --dry-run', 'Do not actually write files, just print output');
 args.option('--types-file <mime.types>', 'Only build resources with the a media type and filename extension listed in the given file');
 args.option('--verbose', 'Output a lot of information');
-//args.option('--delete', 'Delete all other resources under the base');
 args.parse(process.argv);
 
-var appjs = args.f || 'app.js';
-const app = require(path.resolve(appjs));
-
 var verbose = args.verbose;
+var targets = args.args;
+
+if(args.env){
+	if(args.verbose) console.log('Import '+args.env);
+	dotenv.config({ path: path.resolve(process.cwd(), args.env) });
+// }else if(configData.import){
+// 	if(args.verbose) console.log('Import '+configData.import);
+// 	dotenv.config({ path: path.resolve(configFilepath, configData.import) });
+}else{
+	if(args.verbose) console.log('Import .env');
+	dotenv.config();
+}
+
+var appjs = args.f || 'app.js';
+if(verbose) console.error('Load application file: '+appjs);
+const app = require(path.resolve(appjs));
 
 const argbase = args.base && args.base[args.base.length-1]=='/' ? args.base.substring(0, args.base.length-1) : args.base ;
 const defaultBase = (function(){
@@ -39,9 +55,10 @@ if(verbose){
 	console.error('base: '+base);
 }
 
+var mediaTypesMap = {};
+var targetExtensions = new Set;
 if(args.typesFile){
-	var mediaTypesMap = {};
-	var targetExtensions = new Set;
+	if(verbose) console.error('Read mime.types from '+args.typesFile);
 	var typesFileData = fs.readFileSync(args.typesFile, {encoding:'ASCII'});
 	typesFileData.split(/\r?\n/g).map(function(line, i){
 		if(line.match(/^\s*(#.*)?$/)) return;
@@ -65,16 +82,22 @@ function isListedMediaType(mediaType, filename){
 	return mediaTypesMap[mediaType] && mediaTypesMap[mediaType].indexOf(ext)>=0;
 }
 
+if(!args.all && !targets.length && !targetExtensions.size){
+	console.error('No targets specified. Stop.');
+	process.exitCode = 2;
+	return;
+}
+
 app.listing().then(function(list){
 	return Promise.all(list.map(function(params){
 		var uri = typeof params==='string' ? params : app.generateUri(params);
 		if(uri.substring(0, base.length)!==base) return;
 		var relpath = uri.substring(base.length);
 		// Use all files if --all, or any file listed in targets, or any file with an extension in targetExtensions
-		if(!args.all && args.args.indexOf(relpath)<0 && !targetExtensions.has(path.extname(relpath))) return;
+		if(!args.all && targets.indexOf(relpath)<0 && !targetExtensions.has(path.extname(relpath))) return;
 		if(relpath[0]==='/') relpath = relpath.substring(1);
 		if(args.target) relpath = args.target + '/' + relpath;
-		if(args.args.length && args.args.indexOf(relpath)<0) return;
+		if(targets.length && targets.indexOf(relpath)<0) return;
 		var target = args.target ? args.target + '/' + relpath : relpath;
 		var req = {
 			url: uri,
@@ -119,11 +142,11 @@ app.listing().then(function(list){
 					if(args.pretend){
 						console.error('Would write '+relpath+' from <'+uri+'>');
 					}else{
-						console.error(''+relpath+' from <'+uri+'>');
+						console.log(''+relpath+' from <'+uri+'>');
 					}
 					response.pipeBody(stream);
 					return new Promise(function(resolve){
-						stream.on('finish', resolve);
+						stream.on('finish', resolve.bind(null, uri));
 					});
 				}else{
 					console.error('Status code '+response.statusCode, uri);
@@ -132,8 +155,9 @@ app.listing().then(function(list){
 			});
 		});
 	}));
-}).then(function(){
-	if(verbose) console.error('Done');
+}).then(function(list){
+	var built = list.filter(function(v){ return !!v; });
+	if(verbose) console.log('Built '+built.length+' targets');
 }).catch(function(err){
 	console.error('finally', err);
 });
