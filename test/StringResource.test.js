c2354ef0490753ff913fa7f54531516ce0a7d08e
Austin Wright <aaa@bzfx.net>
2019-10-24 10:32:02 -0700

    Add tests for conditional GET requests on StringResource

--- test/StringResource.test.js
+++ test/StringResource.test.js
@@ -12,7 +12,7 @@ describe('StringResource', function(){
 			app.addRoute(new lib.Route({
 				uriTemplate: 'http://localhost/document',
 				prepare: function(){
-					return Promise.resolve(new lib.StringResource(this));
+					return Promise.resolve(new lib.StringResource(this, {}, {ETag: 'etag', lastModified:new Date(1000000000000)}));
 				},
 				renderString: function(){
 					var res = new lib.MessageHeaders;
@@ -75,6 +75,51 @@ describe('StringResource', function(){
 				'Host: localhost',
 			]).then(function(res){
 				assert(res.toString().match(/^HTTP\/1.1 200 /));
+			});
+		});
+		it('200 OK (If-Modified-Since)', function(){
+			return testMessage(app, [
+				'GET http://localhost/document HTTP/1.1',
+				'Host: localhost',
+				'If-Modified-Since: Tue, 28 Aug 2001 12:00:00 GMT',
+			]).then(function(res){
+				assert(res.toString().match(/^HTTP\/1.1 200 /));
+			});
+		});
+		it('200 OK (If-Modified-Since +1 day)', function(){
+			return testMessage(app, [
+				'GET http://localhost/document HTTP/1.1',
+				'Host: localhost',
+				'If-Modified-Since: Wed, 29 Aug 2001 12:00:00 GMT',
+			]).then(function(res){
+				assert(res.toString().match(/^HTTP\/1.1 200 /));
+			});
+		});
+		it('200 OK (If-None-Match)', function(){
+			return testMessage(app, [
+				'GET http://localhost/document HTTP/1.1',
+				'Host: localhost',
+				'If-None-Match: "foo"',
+			]).then(function(res){
+				assert(res.toString().match(/^HTTP\/1.1 200 /));
+			});
+		});
+		it('304 Not Modified (If-Modified-Since)', function(){
+			return testMessage(app, [
+				'GET http://localhost/document HTTP/1.1',
+				'Host: localhost',
+				'If-Modified-Since: Sun, 09 Sep 2001 01:46:40 GMT',
+			]).then(function(res){
+				assert(res.toString().match(/^HTTP\/1.1 304 /));
+			});
+		});
+		it('304 Not Modified (If-None-Match)', function(){
+			return testMessage(app, [
+				'GET http://localhost/document HTTP/1.1',
+				'Host: localhost',
+				'If-None-Match: "etag"',
+			]).then(function(res){
+				assert(res.toString().match(/^HTTP\/1.1 304 /));
 			});
 		});
 	});
