4fb80eeb76e7f050f62f5a3171da192848a62f66
Austin Wright <aaa@bzfx.net>
2019-11-01 00:32:32 -0700

    Improved effective request URI handling

--- test/Servers.test.js
+++ test/Servers.test.js
@@ -17,41 +17,257 @@ describe('HTTPServer (HTTP/1.1)', function(){
 		});
 	});
 	describe('Effective Request URI', function(){
-		it('no options', function(){
-			var app = new lib.Application;
-			app.addRoute(new URIReflect('{+uri}'));
-			var listener = new lib.HTTPServer(app);
-			var server = http.createServer(listener.callback());
-			return writeMessage(server, [
-				'GET / HTTP/1.1',
-				'Host: localhost',
-			]).then(function(res){
-				assert(res.toString().match(/^HTTP\/1.1 200 /));
-				assert(res.toString().match(/^http:\/\/localhost\/$/m));
+		describe('no options', function(){
+			it('origin-form', function(){
+				var app = new lib.Application;
+				app.addRoute(new URIReflect('{+uri}'));
+				var listener = new lib.HTTPServer(app);
+				var server = http.createServer(listener.callback());
+				return writeMessage(server, [
+					'GET / HTTP/1.1',
+					'Host: localhost',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^http:\/\/localhost\/$/m));
+				});
+			});
+			it('origin-form, no Host', function(){
+				var app = new lib.Application;
+				app.addRoute(new URIReflect('{+uri}'));
+				var listener = new lib.HTTPServer(app);
+				var server = http.createServer(listener.callback());
+				return writeMessage(server, [
+					'GET / HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
+			});
+			it('origin-form, multiple Host headers', function(){
+				var app = new lib.Application;
+				app.addRoute(new URIReflect('{+uri}'));
+				var listener = new lib.HTTPServer(app);
+				var server = http.createServer(listener.callback());
+				return writeMessage(server, [
+					'GET / HTTP/1.1',
+					'Host: localhost',
+					'Host: localhost',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
+			});
+			it('absolute-form', function(){
+				var app = new lib.Application;
+				app.addRoute(new URIReflect('{+uri}'));
+				var listener = new lib.HTTPServer(app);
+				var server = http.createServer(listener.callback());
+				return writeMessage(server, [
+					'GET ftp://localhost/ HTTP/1.1',
+					'Host: localhost',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^ftp:\/\/localhost\/$/m));
+				});
+			});
+			it('absolute-form, no Host', function(){
+				var app = new lib.Application;
+				app.addRoute(new URIReflect('{+uri}'));
+				var listener = new lib.HTTPServer(app);
+				var server = http.createServer(listener.callback());
+				return writeMessage(server, [
+					'GET ftp://localhost/ HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
+			});
+			it('absolute-form, no Host, relaxedHost', function(){
+				var app = new lib.Application;
+				app.relaxedHost = true;
+				app.addRoute(new URIReflect('{+uri}'));
+				var listener = new lib.HTTPServer(app);
+				var server = http.createServer(listener.callback());
+				return writeMessage(server, [
+					'GET ftp://localhost/ HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^ftp:\/\/localhost\/$/m));
+				});
+			});
+			it('absolute-form, multiple Host headers', function(){
+				var app = new lib.Application;
+				app.addRoute(new URIReflect('{+uri}'));
+				var listener = new lib.HTTPServer(app);
+				var server = http.createServer(listener.callback());
+				return writeMessage(server, [
+					'GET ftp://localhost/ HTTP/1.1',
+					'Host: localhost',
+					'Host: localhost',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
 			});
 		});
-		it('fixed scheme (origin-form)', function(){
-			var app = new lib.Application;
-			app.fixedScheme = 'ftp';
-			app.addRoute(new URIReflect('{+uri}'));
-			return testMessage(app, [
-				'GET / HTTP/1.1',
-				'Host: localhost:8080',
-			]).then(function(res){
-				assert(res.toString().match(/^HTTP\/1.1 200 /));
-				assert(res.toString().match(/^ftp:\/\/localhost:8080\/$/m));
+		describe('fixed scheme', function(){
+			it('origin-form', function(){
+				var app = new lib.Application;
+				app.fixedScheme = 'ftp';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET / HTTP/1.1',
+					'Host: localhost:8080',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^ftp:\/\/localhost:8080\/$/m));
+				});
+			});
+			it('origin-form, no Host', function(){
+				var app = new lib.Application;
+				app.fixedScheme = 'ftp';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET / HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
+			});
+			it('origin-form, multiple Host headers', function(){
+				var app = new lib.Application;
+				app.fixedScheme = 'ftp';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET http://localhost:8080/ HTTP/1.1',
+					'Host: localhost:8080',
+					'Host: localhost:8080',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
+			});
+			it('absolute-form', function(){
+				var app = new lib.Application;
+				app.fixedScheme = 'ftp';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET http://localhost:8080/ HTTP/1.1',
+					'Host: localhost:8080',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^ftp:\/\/localhost:8080\/$/m));
+				});
+			});
+			it('absolute-form, no Host', function(){
+				var app = new lib.Application;
+				app.fixedScheme = 'ftp';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET http://localhost:8080/ HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
+			});
+			it('absolute-form, no Host, relaxedHost', function(){
+				var app = new lib.Application;
+				app.fixedScheme = 'ftp';
+				app.relaxedHost = true;
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET http://localhost:8080/ HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^ftp:\/\/localhost:8080\/$/m));
+				});
+			});
+			it('absolute-form, no Host, relaxedHost', function(){
+				var app = new lib.Application;
+				app.fixedScheme = 'ftp';
+				app.relaxedHost = true;
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET http://localhost:8080/ HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^ftp:\/\/localhost:8080\/$/m));
+				});
 			});
 		});
-		it('fixed authority (origin-form)', function(){
-			var app = new lib.Application;
-			app.fixedAuthority = 'example.com';
-			app.addRoute(new URIReflect('{+uri}'));
-			return testMessage(app, [
-				'GET / HTTP/1.1',
-				'Host: localhost:8080',
-			]).then(function(res){
-				assert(res.toString().match(/^HTTP\/1.1 200 /));
-				assert(res.toString().match(/^http:\/\/example.com\/$/m));
+		describe('fixed authority', function(){
+			it('origin-form', function(){
+				var app = new lib.Application;
+				app.fixedAuthority = 'example.com';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET / HTTP/1.1',
+					'Host: localhost:8080',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^http:\/\/example.com\/$/m));
+				});
+			});
+			it('origin-form, no Host', function(){
+				var app = new lib.Application;
+				app.fixedAuthority = 'example.com';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET / HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
+			});
+			it('origin-form, multiple Host headers', function(){
+				var app = new lib.Application;
+				app.fixedAuthority = 'example.com';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET / HTTP/1.1',
+					'Host: localhost:8080',
+					'Host: localhost:8080',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
+			});
+			it('absolute-form', function(){
+				var app = new lib.Application;
+				app.fixedAuthority = 'example.com';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET https://localhost:8080/ HTTP/1.1',
+					'Host: localhost:8080',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^https:\/\/example.com\/$/m));
+				});
+			});
+			it('absolute-form, no Host', function(){
+				var app = new lib.Application;
+				app.fixedAuthority = 'example.com';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET https://localhost:8080/ HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
+			});
+			it('absolute-form, no Host, relaxedHost', function(){
+				var app = new lib.Application;
+				app.fixedAuthority = 'example.com';
+				app.relaxedHost = true;
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET https://localhost:8080/ HTTP/1.1',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 200 /));
+					assert(res.toString().match(/^https:\/\/example.com\/$/m));
+				});
+			});
+			it('absolute-form, multiple Host headers', function(){
+				var app = new lib.Application;
+				app.fixedAuthority = 'example.com';
+				app.addRoute(new URIReflect('{+uri}'));
+				return testMessage(app, [
+					'GET https://localhost:8080/ HTTP/1.1',
+					'Host: localhost:8080',
+					'Host: localhost:8080',
+				]).then(function(res){
+					assert(res.toString().match(/^HTTP\/1.1 400 /));
+				});
 			});
 		});
 	});
