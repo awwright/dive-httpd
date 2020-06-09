31aaaa8568b980b17591445d53f2896baca5018a
Austin Wright <aaa@bzfx.net>
2020-06-09 16:33:59 -0700

    mv lib/{RouteStaticFile.js,RouteFilesystem.js}

--- test/meta.test.js
+++ test/meta.test.js
@@ -16,7 +16,7 @@ describe('Meta: Tests exist', function(){
 		assert(methodList.length);
 		assert(interfaceList.length);
 		interfaceList.forEach(function(interfaceName){
-			if(interfaceName==='RouteFilesystem') return;
+			if(interfaceName==='RouteStaticFile') return;
 			methodList.forEach(function(methodName){
 				var expectedTestName = interfaceName+'#'+methodName;
 				function testTest(test){
