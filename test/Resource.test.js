73e7850ce42fb9c09d98a45c7bfacf6d74811a3a
Austin Wright <aaa@bzfx.net>
2020-07-12 13:09:47 -0700

    Implement Resource#error and add tests for 405 Method Not Allowed

--- test/Resource.test.js
+++ test/Resource.test.js
@@ -6,6 +6,58 @@ var testMessage = require('../../dive-httpd/test/util.js').testMessage;
 var lib = require('../../dive-httpd/index.js');
 
 describe('Resource', function(){
+	describe('interface', function(){
+		describe('Resource#error', function(){
+			var route;
+			beforeEach(function(){
+				route = new lib.Route({
+					uriTemplate: 'http://localhost/document',
+					methods: ['GET', 'HEAD', 'OPTIONS'],
+					prepare: function(){
+						return Promise.resolve(new lib.Resource(this));
+					},
+					render: function(){
+						var res = new lib.ResponsePassThrough;
+						res.setHeader('Content-Type', 'text/plain');
+						res.end('The document\r\n');
+						return res;
+					},
+					error: async function(uri, err){
+						return new lib.Resource(this, {
+							render() {
+								var res = new lib.ResponsePassThrough;
+								if(err.statusCode) res.statusCode = err.statusCode;
+								res.setHeader('Content-Type', 'text/plain');
+								res.end('statusCode='+err.statusCode+'\r\n'+err+'\r\n');
+								return res.clientReadableSide;
+							},
+						});
+					},
+				});
+			});
+			it('Resource#error(400)', async function(){
+				const resource = await route.error('http://localhost/document', new lib.errors.ClientError('Generic error'));
+				assert(resource);
+				const res = await lib.ResponseMessage.fromStream(resource.render());
+				assert(res);
+				assert.strictEqual(res.statusCode, 400);
+				assert.match(res.body, /statusCode=400/);
+				assert.match(res.body, /Generic error/);
+			});
+			it('Resource#handle(POST) sends 405', async function(){
+				const resource = await route.prepare('http://localhost/document');
+				assert(resource);
+				const res = await lib.ResponseMessage.fromStream(resource.handle({
+					uri: 'http://localhost/document',
+					method: 'POST',
+					headers: {},
+				}));
+				assert(res);
+				assert.strictEqual(res.statusCode, 405);
+				assert.match(res.body, /statusCode=405/);
+			});
+		});
+	});
 	describe('app', function(){
 		var app;
 		before(function(){
@@ -42,6 +94,30 @@ describe('Resource', function(){
 				},
 				render: function(){
 					throw new Error('Boom 1');
+				},
+			}));
+			app.addRoute(new lib.Route({
+				uriTemplate: 'http://localhost/error/405',
+				methods: ['GET', 'HEAD', 'OPTIONS'],
+				prepare: function(){
+					return Promise.resolve(new lib.Resource(this));
+				},
+				render: function(){
+					var res = new lib.ResponsePassThrough;
+					res.setHeader('Content-Type', 'text/plain');
+					res.end('The document\r\n');
+					return res;
+				},
+				error: async function(uri, err){
+					return new lib.Resource(this, {
+						render() {
+							var res = new lib.ResponsePassThrough;
+							if(err.statusCode) res.statusCode = err.statusCode;
+							res.setHeader('Content-Type', 'text/plain');
+							res.end('statusCode='+err.statusCode+'\r\n'+err+'\r\n');
+							return res.clientReadableSide;
+						},
+					});
 				},
 			}));
 			app.onError = function(){
@@ -82,13 +158,36 @@ describe('Resource', function(){
 				assert.match(res.toString(), /^HTTP\/1.1 200 /);
 			});
 		});
-		it('405 Method Not Allowed', function(){
-			// Tests Resource#custom
-			return testMessage(app, [
-				'PUT http://localhost/document HTTP/1.1',
-				'Host: localhost',
-			]).then(function(res){
-				assert.match(res.toString(), /^HTTP\/1.1 405 /);
+		describe('Custom 405', function(){
+			it('Positive', function(){
+				// Tests Resource#custom
+				return testMessage(app, [
+					'GET http://localhost/error/405 HTTP/1.1',
+					'Host: localhost',
+				]).then(function(res){
+					assert.match(res.toString(), /^HTTP\/1.1 200 /);
+					assert.match(res.toString(), /^The document$/m);
+				});
+			});
+			it('Negative', function(){
+				// Tests Resource#custom
+				return testMessage(app, [
+					'POST http://localhost/error/405 HTTP/1.1',
+					'Host: localhost',
+				]).then(function(res){
+					assert.match(res.toString(), /^HTTP\/1.1 405 /);
+					assert.match(res.toString(), /^statusCode=405$/m);
+				});
+			});
+			it('None', function(){
+				// Tests Resource#custom
+				return testMessage(app, [
+					'POST http://localhost/document HTTP/1.1',
+					'Host: localhost',
+				]).then(function(res){
+					assert.match(res.toString(), /^HTTP\/1.1 405 /);
+					assert.match(res.toString(), /does not implement the POST method/m);
+				});
 			});
 		});
 	});
